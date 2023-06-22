69c3a23751eae9699be22453b1886c12673d245d
silentdiamond <132628168+sefenydiaz@users.noreply.github.com>
2023-06-22 16:10:43 -0400

    finished post route and added delete

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,29 @@
 const router = require('express').Router();
 const fs = require("fs");
+const { v4: uuidv4 } = require('uuid');
+
 
 router.get('/notes', (req, res) => {
     const saveNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
     res.json(saveNotes);
+});
+
+router.post('/notes', (req, res) => {
+    const saveNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+    console.log(req.body)
+    saveNotes.push({ ...req.body, id: uuidv4() })
+    fs.writeFileSync('./db/db.json', JSON.stringify(saveNotes))
+    res.json({ message: "It worked!" })
+})
+
+router.delete('/notes/:id', (req, res) => {
+    const saveNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8'))
+
+
+    const filteredNotes = saveNotes.filter(note => note.id !== req.params.id)
+    console.log(filteredNotes)
+    fs.writeFileSync('./db/db.json', JSON.stringify(filteredNotes))
+    res.json({ message: "It worked!" })
 })
 
 module.exports = router
